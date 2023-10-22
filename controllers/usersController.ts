3bf16b05cd19b8287e0551d555f34bc90069e460
unknown <ruslanchik_nadyrov@mail.ru>
2023-10-22 17:21:37 +0600

    io

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -10,8 +10,22 @@ const prisma = new PrismaClient()
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
-    const users = await prisma.user.findMany()
-    res.send(users)
+    const user = await prisma.user.findMany()
+    res.send(user)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id: number = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+    res.send(user)
   } catch (error) {
     console.log(error)
   }
@@ -62,6 +76,20 @@ export const authUser = async (req: Request, res: Response): Promise<void> => {
     } else {
       throw new Error('Пользователь не найден')
     }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id: number = parseInt(req.params.id)
+    const user = await prisma.user.delete({
+      where: {
+        id
+      }
+    })
+    res.send(user)
   } catch (error) {
     console.log(error)
   }
--- routers/usersRouter.ts
+++ routers/usersRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { getUsers, addUser, authUser } from '../controllers/usersController'
+import { getUser, addUser, authUser, getUsers, deleteUser } from '../controllers/usersController'
 
 const router = Router()
 
+router.get('/api/users/:id', getUser)
 router.get('/api/users', getUsers)
 router.post('/api/users', addUser)
 router.post('/api/auth', authUser)
+router.delete('/api/users/:id', deleteUser)
 
 export default router
