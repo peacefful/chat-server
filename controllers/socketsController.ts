4befe10230b7a0bbc5d11a19fd5035cbae55aa0e
unknown <ruslanchik_nadyrov@mail.ru>
2024-01-21 16:12:10 +0600

    join chat message

--- controllers/socketsController.ts
+++ controllers/socketsController.ts
@@ -1,24 +1,39 @@
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 
 const sockets = (io: Server) => {
 	io.on('connection', (socket) => {
 		socket.on('join', (room) => {
 			socket.join(room);
-			console.log(`User ${socket.id} joined room ${room}`);
+			console.log(`User joined room ${room}`);
 		});
-		
+
+		socket.on('userJoin', (room, user) => {
+			socket.broadcast.to(room).emit('userJoin', user)
+		})
+
 		socket.on('message', (text, id, sendTime, uuid, username) => {
-			io.to(uuid).emit('message', text, id, sendTime, uuid, username);
-			console.log(text, id, sendTime, username);
+			io.to(uuid).emit('message',
+				text,
+				id,
+				sendTime,
+				uuid,
+				username
+			);
+			console.log(text, id, username);
 		});
-	
+
 		socket.on('personalInvite', (uuid) => {
 			socket.join(uuid);
 			console.log(`User joined room ${uuid}`);
 		});
-	
+
 		socket.on('messageInvite', (data) => {
-			socket.broadcast.to(data.userUuid).emit('messageInvite', data.uuidRoom, data.titleRoom, data.userUuid);
+			socket.broadcast.to(data.userUuid)
+				.emit('messageInvite',
+					data.uuidRoom,
+					data.titleRoom,
+					data.userUuid
+				);
 			console.log(`Пользователь ${data.userUuid} приглашен в комнату ${data.titleRoom} [${data.uuidRoom}]`);
 		});
 	});
